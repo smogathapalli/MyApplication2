f67d2a79e6d5dd8901e835558cd32223445f4a73
smogathapalli <mskchakravarthi@gmail.com>
2022-05-19 17:33:29 +0530

    Commit 1

--- app/src/main/java/com/example/smogathapalli/myapplication/MainActivity.java
+++ app/src/main/java/com/example/smogathapalli/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int i = 5;
+        int i = 55;
 
         int ie = 2;
 
